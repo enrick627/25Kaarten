98d42a6afa8ed83499c72cd8612d982dc6b73f6e
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-12-04 10:05:39 +0100

    2de commit

--- Kaarten/Program.cs
+++ Kaarten/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Enrick De Munter nr3 4/12/19 kaarten 5ITN
+//v1: 2019/12/4 - demo voor emu
+/*
+ * 
+ * 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +14,35 @@ namespace Kaarten
 {
     class Program
     {
+        enum Soort
+        {
+            Harten, 
+            Klaveren,
+            Ruiten,
+            schoppen
+
+        }
+
         static void Main(string[] args)
         {
+            Soort soort;
+            soort = Soort.Klaveren;
+            string invoer;
+            string soorttekst = soort.ToString();
+
+
+            Console.WriteLine("wat is uw kaartsoort? : ");
+            invoer = Console.ReadLine();
+
+            soort = Enum.Parse(soort,invoer);
+
+
+            Console.WriteLine($"de soort van de kaart is {soort.ToString().ToLower()}.");
+
+            //wachten
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten:");
+            Console.ReadKey();
         }
     }
 }
